82d9a69428e50729e53aeddf3e03becfe48becbc
Your Name <you@example.com>
2025-03-21 19:39:12 +0100

    add js file, add some css modules, format etc.

---
+++ index.js
@@ -0,0 +1,13 @@
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const birthday = document.getElementById('birthday');
+const gender = document.getElementById('gender');
+const fname = document.querySelector("#fname");
+const sname = document.getElementById('sname');
+
+
+function handleButtonClick() {
+    console.log(document.querySelector("#fname").value);
+}
